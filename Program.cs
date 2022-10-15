c27d123ca4bc7f9623027695fb7f408b8df6fdac
Joan Magnet <jmagnet@vilardellpurti.com>
2022-10-15 11:39:47 +0200

    working on feature2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, this is the original program.!");
 
-Console.WriteLine("Feature2 is amazing.")
+Console.WriteLine("Feature2 is amazing.");
+
+Console.WriteLine("More things about feature2.");
