380376f739b5b04a7229f7290f32a7d15545ae31
Joan Magnet <jmagnet@vilardellpurti.com>
2022-10-15 11:47:38 +0200

    all good on main

--- Program.cs
+++ Program.cs
@@ -3,5 +3,6 @@ Console.WriteLine("Hello, this is the original program.!");
 
 Console.WriteLine("Still working on main.");
 Console.WriteLine("Added a new feature.");
-
 Console.WriteLine("Added more things to feature1");
+
+Console.WriteLine("More work on main.");
